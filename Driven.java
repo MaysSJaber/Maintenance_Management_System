fa01dd89dde5c13b1ab85b90ec1e8697ec712550
MaysJaber <maysjaberr@gmail.com>
2026-09-09 23:56:14 +0300

    Add files via upload

---
+++ Driven.java
@@ -0,0 +1,24 @@
+
+import Core.Person;
+import Core.User;
+import Screens.*;
+
+import java.io.IOException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author maysj
+ */
+public class Driven {
+
+    public static void main(String[] args) throws IOException {
+
+
+
+    } 
+}
