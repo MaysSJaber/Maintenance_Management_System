6fdb58611d8ca33e7c6db720e73a6f8f88e7ad16
MaysJaber <maysjaberr@gmail.com>
2026-09-09 23:53:08 +0300

    Add files via upload

---
+++ Core/Person.java
@@ -0,0 +1,71 @@
+package Core;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author maysj
+ */
+public abstract class Person {
+    
+   private String name;
+   private int age;
+   private String address;
+   private String phone;
+   private String email;
+
+    public Person(String name, int age, String address, String phone, String email) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+}
